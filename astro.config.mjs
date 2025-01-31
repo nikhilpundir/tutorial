23fb43aabb82966f4e32dddf37e180d67474ce25
Nikhil Pundir <nroyalrana@gmail.com>
2025-02-01 00:26:44 +0530

    my first island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-     site: "https://tutorial-nikhilpundir.netlify.app/"
+  site: "https://tutorial-nikhilpundir.netlify.app/",
+  integrations: [preact()]
 });
